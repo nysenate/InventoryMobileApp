ee99caba4122f788b9a2e5d43157c2b2b01e5e5f
Brian <heitner@nysenate.gov>
2013-05-06 12:01:14 -0400

    Added SenateActivity as a base for our Activities to help reduce repetitive code.

---
+++ src/gov/nysenate/inventory/android/SenateActivity.java
@@ -0,0 +1,38 @@
+package gov.nysenate.inventory.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public abstract class SenateActivity extends Activity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            Toast toast = Toast.makeText(getApplicationContext(), "Going Back",
+                    Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+            NavUtils.navigateUpFromSameTask(this);
+
+            overridePendingTransition(R.anim.in_left, R.anim.out_right);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }    
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.in_left, R.anim.out_right);
+    }    
+    
+}
