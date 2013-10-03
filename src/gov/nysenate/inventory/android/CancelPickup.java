399748c4e2b1a85ca995832864449975f4562d03
nilangshus <nilangshudas2010@gmail.com>
2013-10-03 12:29:50 -0400

    Update CancelPickup.java

--- src/gov/nysenate/inventory/android/CancelPickup.java
+++ src/gov/nysenate/inventory/android/CancelPickup.java
@@ -23,7 +23,9 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+
 public class CancelPickup extends SenateActivity {
+    ///testing git
 
     private Pickup pickup;
 
