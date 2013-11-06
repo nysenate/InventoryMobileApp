1b71bf25fb7519ec58b3fd33a339733c71eb429d
Kevin Caseiras <kcaseiras@gmail.com>
2013-11-06 12:54:04 -0500

    Add Remote Transaction info.

--- src/gov/nysenate/inventory/model/Transaction.java
+++ src/gov/nysenate/inventory/model/Transaction.java
@@ -9,6 +9,22 @@ public abstract class Transaction implements Parcelable
     protected int nuxrpd;
     protected Location origin;
     protected Location destination;
+    private int shipId;
+    private String shipType;
+    private String shipComments;
+    private int verificationId;
+    private String verificationMethod;
+    private String verificationComments;
+    private int employeeId;
+    private int helpReferenceNum;
+
+    // shipType must exists for all remote transactions.
+    public boolean isRemote() {
+        if (shipType != null) {
+            return true;
+        }
+        return false;
+    }
 
     protected Transaction() {
         origin = new Location();
@@ -74,6 +90,70 @@ public abstract class Transaction implements Parcelable
 
     public String getDestinationSummaryString() {
         return destination.getLocationSummaryString();
+    }
+
+    public int getShipId() {
+        return shipId;
+    }
+
+    public void setShipId(int shipId) {
+        this.shipId = shipId;
+    }
+
+    public String getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(String shipType) {
+        this.shipType = shipType;
+    }
+
+    public String getShipComments() {
+        return shipComments;
+    }
+
+    public void setShipComments(String shipComments) {
+        this.shipComments = shipComments;
+    }
+
+    public int getVerificationId() {
+        return verificationId;
+    }
+
+    public void setVerificationId(int verificationId) {
+        this.verificationId = verificationId;
+    }
+
+    public String getVerificationMethod() {
+        return verificationMethod;
+    }
+
+    public void setVerificationMethod(String verificationMethod) {
+        this.verificationMethod = verificationMethod;
+    }
+
+    public String getVerificationComments() {
+        return verificationComments;
+    }
+
+    public void setVerificationComments(String verificationComments) {
+        this.verificationComments = verificationComments;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getHelpReferenceNum() {
+        return helpReferenceNum;
+    }
+
+    public void setHelpReferenceNum(int helpReferenceNum) {
+        this.helpReferenceNum = helpReferenceNum;
     }
 
     // ---------- Code for Parcelable interface ----------
