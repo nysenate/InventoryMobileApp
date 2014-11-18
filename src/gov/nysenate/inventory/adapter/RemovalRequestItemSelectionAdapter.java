5174dfa81645929dfdee8ddb677d3e1b3864f312
Brian Heitner <heitner@nysenate.gov>
2014-11-18 10:11:31 -0500

    Sort the removal requests to match SFMS Removal Request Screen refs #8230

--- src/gov/nysenate/inventory/adapter/RemovalRequestItemSelectionAdapter.java
+++ src/gov/nysenate/inventory/adapter/RemovalRequestItemSelectionAdapter.java
@@ -10,6 +10,8 @@ import gov.nysenate.inventory.android.R;
 import gov.nysenate.inventory.model.Item;
 import gov.nysenate.inventory.model.ItemStatus;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class RemovalRequestItemSelectionAdapter extends ArrayAdapter<Item>
@@ -20,11 +22,13 @@ public class RemovalRequestItemSelectionAdapter extends ArrayAdapter<Item>
 
     private List<Item> items;
     private RemovalRequestItemSelectionAdapterI handler;
+    RemovalRequestComparer removalRequestComparer = new RemovalRequestComparer();
 
     public RemovalRequestItemSelectionAdapter(Context context, int resourceId, int textViewResource,
                                               List<Item> items, RemovalRequestItemSelectionAdapterI handler) {
         super(context, resourceId, textViewResource, items);
         this.items = items;
+        Collections.sort(items, removalRequestComparer);
         this.handler = handler;
     }
 
@@ -67,4 +71,51 @@ public class RemovalRequestItemSelectionAdapter extends ArrayAdapter<Item>
 
         return row;
     }
+    
+    public class RemovalRequestComparer implements Comparator<Item> {
+        @Override
+        public int compare(Item item1, Item item2) {
+            String description1 = "";
+            
+            try {
+                description1 = item1.getCommodity().getDescription();
+            }
+            catch (Exception e) {
+                
+            }
+            
+            String description2 = "";
+            try {
+                description2 = item2.getCommodity().getDescription();
+            }
+            catch (Exception e) {
+                
+            }
+            
+            int value1 = description1.compareTo(description2);
+            if (value1 == 0) {
+                int serialNumber1 = 0;
+                try {
+                    serialNumber1 = Integer.parseInt(item1.getSerialNumber());
+                }
+                catch (Exception e) {
+                    
+                }
+                
+                int serialNumber2 = 0;
+                try {
+                    serialNumber2 = Integer.parseInt(item2.getSerialNumber());
+                }
+                catch (Exception e) {
+                    
+                }
+                
+                int value2 = serialNumber1 - serialNumber2;
+                
+                return value2;
+            }
+            return value1;
+        }
+
+      }    
 }
